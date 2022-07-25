f1e5e36a5863a5616ce159ac941716cd98b17d53
mohith <mohit@tridemobility.com>
2022-07-25 21:36:37 +0530

    asa

---
+++ android/app/src/main/kotlin/com/example/barber_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.barber_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  DatePickerController _controller = DatePickerController();
+
+
+  formattedDate(date) {
+    final DateTime convertedDate = DateTime.parse(date);
+    final DateFormat formatter = DateFormat('dd-MM-yyyy');
+    final String formatted = formatter.format(convertedDate);
+    print(formatted);
+    return formatted;
+    // something like 2013-04-20
+  }
+
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children:[
+          Container(
+            child:Card(
+              child: DatePicker(
+                DateTime.now(),
+                width: 60,
+                height: 80,
+                controller: _controller,
+                initialSelectedDate: DateTime.now(),
+                selectionColor: Colors.grey,
+                selectedTextColor: Colors.white,
+                // inactiveDates: [
+                //   DateTime.now().add(Duration(days: 3)),
+                //   DateTime.now().add(Duration(days: 4)),
+                //   DateTime.now().add(Duration(days: 7))
+                // ],
+                onDateChange: (date) {
+                  // New date selected
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+
+              )
+            )
+          ),
+
+        ]
+      )
+    );
+  }
+}
